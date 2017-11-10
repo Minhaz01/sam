efc7412d2d088743f9e80daec3a6ef5a46c43945
Minhaz01 <33205845+Minhaz01@users.noreply.github.com>
2017-11-09 20:09:33 -0600

    Update parents.java
    hay

--- Project10/src/proj10/parents.java
+++ Project10/src/proj10/parents.java
@@ -4,7 +4,10 @@ public class parents {
 
 	public static void main(String[] args) {
 		System.out.println(" Try untill you get it");
-
+		System.out.println(" Try untill you get it - Hurray I am from remote repo");
+		System.out.println(" Try untill you get it -  Hurray I am from remote repo");
+		System.out.println(" Try untill you get it -  Hurray  I am from remote repo");
+		System.out.println(" Try untill you get it - Hurray I am from remote repo");
 	}
 
 }
